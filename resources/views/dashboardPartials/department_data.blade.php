cd25c82549b38ebb9f1745ab92bc97f09b366b5e
mozhulungdsuo <mozhui.lungdsuo@gmail.com>
2022-09-16 12:01:37 +0530

    made chnages

--- resources/views/dashboardPartials/department_data.blade.php
+++ resources/views/dashboardPartials/department_data.blade.php
@@ -1,39 +1,42 @@
 <div class="col-md-12">
-  <div class="card">
-    <div class="card-header">
-      <h5 class="card-title">District Wise Data</h5>
-    </div>
-    <div class="card-body">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="form-group row">
-            <label class="col-lg-3 control-label pt-2" for="inputDefault"><b>Select Department</b></label>
-            <div class="col-lg-6">
-              <select class="form-control" id="inputDefault">
-                <option value="">Select</option>
-                <option value="Animal Husbandry">Animal Husbandry</option>
-                <option value="Agriculture">Agriculture</option>
-                <option value="Horticulture">Horticulture</option>
-              </select>
+    <div class="card">
+        <div class="card-header">
+            <h5 class="card-title">District Wise Data</h5>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group row">
+                        <label class="col-lg-3 control-label pt-2" for="inputDefault"><b>Select District</b></label>
+                        <div class="col-lg-6">
+                            <select class="form-control" id="inputDefault">
+                                <option value="">Select</option>
+                                <option value="Kohima">Kohima</option>
+                                <option value="Dimapur">Dimapur</option>
+                                <option value="Mokokchung">Mokokchung</option>
+                                <option value="Mon">Mon</option>
+                                <option value="Tuensang">Tuensang</option>
+                                <option value="Wokha">Wokha</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 text-right">
+                    <button class="btn btn-primary btn-sm"><i class="fas fa-download"></i> Export as Excel</button>
+                </div>
             </div>
-          </div>
+            <table class="table table-striped mt-4">
+                <thead>
+                    <tr>
+                        <th>Scheme name</th>
+                        <th>Fund Allocated</th>
+                        <th>Fund Utilised</th>
+                        <th>Department</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+            </table>
         </div>
-        <div class="col-md-6 text-right">
-          <button class="btn btn-primary btn-sm"><i class="fas fa-download"></i> Export as Excel</button>
-        </div>
-      </div>
-      <table class="table table-striped mt-4">
-        <thead>
-          <tr>
-            <th>Scheme name</th>
-            <th>Fund Allocated</th>
-            <th>Fund Utilised</th>
-            <th>Department</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-      </table>
     </div>
-  </div>
 
 </div>
