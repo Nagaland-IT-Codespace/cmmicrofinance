9453e39d52caab1f6a16a019ee9c842dfd12e19c
mozhulungdsuo <mozhui.lungdsuo@gmail.com>
2022-10-18 11:48:30 +0530

    made hcnages

---
+++ app/Http/Controllers/SentMailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SentMail;
+use Illuminate\Http\Request;
+
+class SentMailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SentMail  $sentMail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SentMail $sentMail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SentMail  $sentMail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SentMail $sentMail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SentMail  $sentMail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SentMail $sentMail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SentMail  $sentMail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SentMail $sentMail)
+    {
+        //
+    }
+}
