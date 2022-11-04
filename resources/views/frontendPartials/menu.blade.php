cd94bf0d4b56ef1411eabbbafe799936d543bbb6
yangeruzi <yangeruzi@gmail.com>
2022-11-04 11:21:59 +0530

    sdffd

--- resources/views/frontendPartials/menu.blade.php
+++ resources/views/frontendPartials/menu.blade.php
@@ -1,17 +1,9 @@
 <nav id="navbar" class="navbar">
   <ul class="bi bi-list">
     <li><a class="nav-link" href="{{url('/')}}">Home</a></li>
-<<<<<<< HEAD
     <li><a class="nav-link scrollto" href="#about">About</a></li>
     <li><a class="nav-link scrollto" href="#services">Downloads</a></li>
     <li><a class="nav-link" href="{{route('contact')}}">Contact Us</a></li>
-=======
-    <li><a class="nav-link scrollto" href="{{url('/')}}#about">About</a></li>
-    <li><a class="nav-link scrollto" href="{{url('/')}}#services">Downloads</a></li>
-    <li><a class="nav-link" href="{{route('notifications')}}">Notifications</a></li>
-    <li><a class="nav-link scrollto" href="{{url('/')}}#services">Gallery</a></li>
-    <li><a class="nav-link" href="{{route('contact')}}">Contact</a></li>
->>>>>>> 6e46598496d90112b92808a15a1f141347d939b5
     <li>
       @auth
       <a class="getstarted scrollto" href={{url('/login')}}>Dashboard</a>
