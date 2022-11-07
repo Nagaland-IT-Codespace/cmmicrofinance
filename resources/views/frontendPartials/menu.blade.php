f415dc54cbfadf3eb5e602eae829c303271e5d09
mozhulungdsuo <mozhui.lungdsuo@gmail.com>
2022-11-07 21:17:47 +0530

    made chnages

--- resources/views/frontendPartials/menu.blade.php
+++ resources/views/frontendPartials/menu.blade.php
@@ -1,18 +1,24 @@
 <nav id="navbar" class="navbar">
-  <ul>
-    <li><a class="nav-link" href="{{url('/')}}">Home</a></li>
-    <li><a class="nav-link scrollto" href="#about">About</a></li>
-    <li><a class="nav-link scrollto" href="#services">Downloads</a></li>
-    <li><a class="nav-link" href="{{route('contact')}}">Contact Us</a></li>
-    <li><a class="nav-link" href="{{route('notifications')}}">Notifications</a></li>
-    <li>
-      @auth
-      <a class="scrollto nav-link" href={{url('/login')}}>Dashboard</a>
-      @endauth
-      @guest
-      <a class="scrollto nav-link" href={{url('/login')}}>Login</a>
-      @endguest
-    </li>
-  </ul>
-  <i class="bi bi-list mobile-nav-toggle"></i>
+    <ul>
+        <li><a class="nav-link" href="{{ url('/') }}">Home</a></li>
+        {{-- if not in home page --}}
+        @if (Request::path() != '/')
+            <li><a class="nav-link" href="{{ url('/') }}#about">About</a></li>
+            <li><a class="nav-link" href="{{ url('/') }}#services">Downloads</a></li>
+        @else
+            <li><a class="nav-link scrollto" href="#about">About</a></li>
+            <li><a class="nav-link scrollto" href="#services">Downloads</a></li>
+        @endif
+        <li><a class="nav-link" href="{{ route('contact') }}">Contact Us</a></li>
+        <li><a class="nav-link" href="{{ route('notifications') }}">Notifications</a></li>
+        <li>
+            @auth
+                <a class="scrollto nav-link" href={{ url('/login') }}>Dashboard</a>
+            @endauth
+            @guest
+                <a class="scrollto nav-link" href={{ url('/login') }}>Login</a>
+            @endguest
+        </li>
+    </ul>
+    <i class="bi bi-list mobile-nav-toggle"></i>
 </nav><!-- .navbar -->
